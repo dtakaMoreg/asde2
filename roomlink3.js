334dae55575171827d7b84505515f6c7fbdf30ab
dai <dai@dai>
2024-10-06 01:21:23 +0900

    elements

--- roomlink3.js
+++ roomlink3.js
@@ -8,24 +8,25 @@ javascript:(function() {
     if (window.location.href === targetUrl) {
       // local_storage_click_countの取得、存在しない場合は0に設定
       let clickCount = parseInt(localStorage.getItem(localStorageKeyCount)) || 0;
-  
-      // elements_aとelements_bを取得
-      const divs = document.querySelectorAll('div[class*="masonry-grid-column"]');
-      let elements_a = [];
-      let elements_b = [];
-  
-      if (divs[0]) {
-        const links_a = divs[0].querySelectorAll('a[class*="link-image"]');
-        links_a.forEach(link => elements_a.push(link));
-      }
-  
-      if (divs[1]) {
-        const links_b = divs[1].querySelectorAll('a[class*="link-image"]');
-        links_b.forEach(link => elements_b.push(link));
-      }
-  
+
       // clickCount個目のelements_aが存在するか確認
       function clickElementIfExists() {
+        
+        // elements_aとelements_bを取得
+        const divs = document.querySelectorAll('div[class*="masonry-grid-column"]');
+        let elements_a = [];
+        let elements_b = [];
+    
+        if (divs[0]) {
+          const links_a = divs[0].querySelectorAll('a[class*="link-image"]');
+          links_a.forEach(link => elements_a.push(link));
+        }
+    
+        if (divs[1]) {
+          const links_b = divs[1].querySelectorAll('a[class*="link-image"]');
+          links_b.forEach(link => elements_b.push(link));
+        }
+
         if (clickCount < elements_b.length) {
           // elements_a[clickCount]が存在する場合はクリック
           elements_b[clickCount].click();
