18673a54caa4804c6a177a6cf6649f1e48ca083d
dai <dai@dai>
2024-11-10 10:57:39 +0900

    1000

--- follower_get_list.js
+++ follower_get_list.js
@@ -1,6 +1,6 @@
 javascript:(function() {
 
-    const MAX_DIV_TAGS = 500;  // 定数として300を設定
+    const MAX_DIV_TAGS = 1000;
 
     // 最初にfollow-buttonを含むclass名のbuttonをクリックする
     function clickFollowButton() {
