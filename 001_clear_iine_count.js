926a957e4f4407fefa8e00e1884fc7c12dbfc695
dai <dai@dai>
2025-01-12 10:24:31 +0900

    room_click

--- 001_clear_iine_count.js
+++ 001_clear_iine_count.js
@@ -1,6 +1,23 @@
-//TamperMonkeyでいいねした数をlocalStorageの[rakuten_click_count]に入れている
+//TamperMonkeyでいいねした数をlocalStorageの[room_click_yyyymmddhhmmss]に入れている
 //それをクリアするブックマークレット
-javascript: (function() {
-    localStorage.setItem('rakuten_click_count', '0');
-    alert('rakuten_click_count has been reset to 0');
+javascript:(function() {
+    try {
+        // Loop through localStorage and collect keys starting with "room_click_"
+        var keysToDelete = [];
+        for (var i = 0; i < localStorage.length; i++) {
+            var key = localStorage.key(i);
+            if (key.startsWith('room_click_')) {
+                keysToDelete.push(key);
+            }
+        }
+
+        // Delete the collected keys
+        keysToDelete.forEach(function(key) {
+            localStorage.removeItem(key);
+        });
+
+        alert('「room_click_」で始まるすべてのキーを削除しました: ' + keysToDelete.length + '件');
+    } catch (e) {
+        alert('エラーが発生しました: ' + e.message);
+    }
 })();
--- 002_get_iine_count.js
+++ 002_get_iine_count.js
@@ -1,19 +1,25 @@
-//TamperMonkeyでいいねした数をlocalStorageの[rakuten_click_count]に入れている
-//それを取得してクリップボードにコピーするブックマークレット
+//TamperMonkeyでいいねした数をlocalStorageの[room_click_yyyymmddhhmmss]に入れている
+//その数を取得してクリップボードにコピーするブックマークレット
 
 javascript:(function() {
     try {
-        var value = localStorage.getItem('rakuten_click_count');
-        if (value === null) {
-            alert('rakuten_click_count が見つかりませんでした。');
-        } else {
-            navigator.clipboard.writeText(value).then(function() {
-                alert('クリップボードにコピーしました: ' + value);
-            }).catch(function(err) {
-                alert('クリップボードへのコピーに失敗しました: ' + err);
-            });
+        // Count the number of localStorage keys starting with "room_click_"
+        var count = 0;
+        for (var i = 0; i < localStorage.length; i++) {
+            var key = localStorage.key(i);
+            if (key.startsWith('room_click_')) {
+                count++;
+            }
         }
+
+        // Copy the count to clipboard
+        navigator.clipboard.writeText(count.toString()).then(function() {
+            alert('「room_click_」で始まるキーの数をクリップボードにコピーしました: ' + count);
+        }).catch(function(err) {
+            alert('クリップボードへのコピーに失敗しました: ' + err);
+        });
     } catch (e) {
         alert('エラーが発生しました: ' + e.message);
     }
 })();
+
